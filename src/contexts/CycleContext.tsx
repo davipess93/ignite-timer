da38e12648bee4f86efe4315eda60b9bdf50a055
Davi Pessoa <davialvespessoa@gmail.com>
2023-03-28 09:57:06 -0300

    Utilizando immer

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -67,7 +67,7 @@ export function CyclesContextProvider({
   }
 
   function interruptCurrentCycle() {
-    dispatch(interruptCurrentCycleAction)
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
